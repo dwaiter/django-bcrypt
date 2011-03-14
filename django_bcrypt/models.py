31fe81b99f004a63cc004902a203768fa18994af
Simon Law <SimonL@trustcentric.com>
2011-03-14 17:43:45 -0400

    Save original User.set_password.

--- django_bcrypt/models.py
+++ django_bcrypt/models.py
@@ -1,16 +1,20 @@
 import bcrypt
+
 from django.contrib.auth.models import User
 from django.conf import settings
 
 
 rounds = getattr(settings, "BCRYPT_ROUNDS", 12)
-_check_password = User.check_password
+
 
 def bcrypt_check_password(self, raw_password):
     if self.password.startswith('bc$'):
         salt_and_hash = self.password[3:]
         return bcrypt.hashpw(raw_password, salt_and_hash) == salt_and_hash
     return _check_password(self, raw_password)
+_check_password = User.check_password
+User.check_password = bcrypt_check_password
+
 
 def bcrypt_set_password(self, raw_password):
     if raw_password is None:
@@ -18,7 +22,5 @@ def bcrypt_set_password(self, raw_password):
     else:
         salt = bcrypt.gensalt(rounds)
         self.password = 'bc$' + bcrypt.hashpw(raw_password, salt)
-
-User.check_password = bcrypt_check_password
+_set_password = User.set_password
 User.set_password = bcrypt_set_password
-
